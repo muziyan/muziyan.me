bc2bd9d78d6949beb74290bad458d8d75fb68696
season <997432833lixuan@gmail.com>
2023-08-14 22:07:25 +0800

    chore: delpoy to netify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://muziyan.me',
-	integrations: [mdx(), sitemap(),react()],
+  site: 'https://muziyan.me',
+  integrations: [mdx(), sitemap(), react()],
+  output: "server",
+  adapter: netlify()
 });
